27d478beaf8c5ecca5a4a7e0f72e12fb2c806ba7
Tushar <tushar@apples-MacBook-Pro.local>
2019-07-09 14:26:07 +0530

    Initial commmit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap().then( () => {
+  Logger.log('Server listning at port 3000');
+});
